9b2814e007f3dad7c6b93bbcc0eaa1943e4dec59
AndreyMouraGouvea <andreygouveia.05@gmail.com>
2022-08-04 09:24:14 -0300

    layout done

---
+++ README.md
@@ -0,0 +1,6 @@
+# Weather API
+
+- https://reactnative.dev/docs/getting-started
+- https://reactnavigation.org/docs/getting-started/
+- https://docs.expo.dev/
+- https://console.hgbrasil.com/documentation/weather
---
+++ src/Pages/Home.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+
+function Home() {
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.component}>
+                <Text style={styles.text}>Previsão do Tempo</Text>
+            </View>
+            <View style={styles.component}>
+                <TextInput style={styles.input}
+                    placeholder='Digite sua cidade'
+                    placeholderTextColor='#FFF'
+                />
+            </View>
+            <View style={styles.component}>
+                <TouchableOpacity>
+                    <Text style={styles.text}>Buscar</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#000'
+    },
+    component: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '90%',
+        backgroundColor: 'purple',
+        padding: 10,
+        marginBottom: 10,
+        borderRadius: 10
+    },
+    input: {
+        fontSize: 20,
+        color: '#FFF'
+    },
+    text: {
+        fontSize: 20,
+        color: '#FFF'
+
+    }
+})
+
+export default Home
